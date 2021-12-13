3fae8afd8cc9eb46bf2a99678569eb6b240873b2
Utkarsh736 <utkarshtomar736@gmail.com>
2021-12-13 19:34:30 +0530

    Function update

--- main.js/app.js
+++ main.js/app.js
@@ -8,7 +8,7 @@ function computerPlay(){
     let options = ["rock", "paper", "scissor"]
     let computerSelection = options[Math.floor(Math.random(options)*options.length)]
 
-    console.log(computerSelection)
+    // console.log("Comp1: " + computerSelection)
     return(computerSelection)
     
 }
@@ -16,32 +16,55 @@ function computerPlay(){
 // Taking user's input
 
 let getInput = prompt("Enter you option")
-let playerSelection  = getInput.toLowerCase()
-
+let flag = 0
+if (getInput == null){
+    console.log("Try Again")
+}
+else{
+    flag = 1
+    
+}
 
 // Function to display result of one round
+if (flag == 1){
+    let playerSelection  = getInput.toLowerCase()
 
-function main(playerSelection, computerSelection){
+    function main(playerSelection, computerSelection){
 
-    switch(playerSelection + computerSelection){
-        case "rockrock":
-        case "paperpaper":
-        case "scissorscissor":
-            return("Draw")
-            break
-        
-        case "rockscissor" :
-        case "paperrock":
-        case "scissorpaper":
-            return("Win")
-            break
-        
-        case "rockpaper" :
-        case "paperscissor":
-        case "scissorrock":
-            return("Lose")
-            break 
+        switch(playerSelection + computerSelection){
+            case "rockrock":
+            case "paperpaper":
+            case "scissorscissor":
+                return("Draw")
+                break
+            
+            case "rockscissor" :
+            case "paperrock":
+            case "scissorpaper":
+                return("Win")
+                break
+            
+            case "rockpaper" :
+            case "paperscissor":
+            case "scissorrock":
+                return("Lose")
+                break 
+        }
+    }
+
+    console.log("comp2: " + computerPlay())
+    // console.log(main(playerSelection, computerPlay()))
+    console.log("comp3: " + computerPlay())
+}
+
+
+function game(){
+    for(let i=0; i<=5; i++){
+        main(playerSelection, computerPlay())
     }
 }
 
-console.log(main(playerSelection, computerPlay()))
+game()
+
+
+
