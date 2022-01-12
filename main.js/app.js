6574132edd186bf6179df26cad46b17bd4f8ccb0
Utkarsh736 <utkarshtomar736@gmail.com>
2022-01-12 15:21:56 +0530

    UI Setup

--- main.js/app.js
+++ main.js/app.js
@@ -1,5 +1,6 @@
 let userScore = 0;
 let compScore = 0;
+
 const rock_button = document.getElementById('r');
 const paper_button = document.getElementById('p');
 const scissors_button = document.getElementById('s');
@@ -10,60 +11,75 @@ const result_p = document.querySelector('.result > p');
 // computerPlay Function
 
 function computerPlay(){
-    let options = ["r", "p", "s"]
-    let computerChoice = options[Math.floor(Math.random(options)*options.length)]
+    let options = ["r", "p", "s"];
+    let computerSelection = options[Math.floor(Math.random(options)*options.length)];
 
-    return(computerChoice)
+    return(computerSelection);
 
+}
+
+
+function letterConverter(letter){
+    if(letter === "r")return 'Rock';
+    if(letter === "p")return 'Paper';
+    return 'Sciccors';
 }
 
 // Functions for the result
 
-function win(){
+function win(userChoice, compChoice){
     userScore++;
     user_span.innerHTML = userScore;
     comp_span.innerHTML = compScore;
-    result_p.innerHTML = `Win`;
+    result_p.innerHTML = `${letterConverter(userChoice)} beats ${letterConverter(compChoice)}. You Win`;
+
+    if(userScore > 5){
+        gameWinner('User');
+    }
+
 };
 
-function lose(){
+function lose(userChoice, compChoice){
     compScore++;
     user_span.innerHTML = userScore;
     comp_span.innerHTML = compScore;
-    result_p.innerHTML = `Lost`;
+    result_p.innerHTML = `${letterConverter(userChoice)} loses to ${letterConverter(compChoice)}. You Lost`;
+
+    if(compScore > 5){
+        gameWinner('Comp');
+    };
 };
 
-function draw(){
+function draw(userChoice, compChoice){
     user_span.innerHTML = userScore;
     comp_span.innerHTML = compScore;
-    result_p.innerHTML = `Draw`;
+    result_p.innerHTML = `${letterConverter(userChoice)} equals ${letterConverter(compChoice)}. It's a Draw`;
 };
-
 
 // Function to display result of one round
 
-function playRound(playerSelection){
+function playRound(userChoice){
 
-    let computerSelection = computerPlay();
+    let compChoice = computerPlay();
 
-    switch(playerSelection + computerSelection){
+    switch(userChoice + compChoice){
         
         case "rr":
         case "pp":
         case "ss":
-            draw();
+            draw(userChoice, compChoice);
             break
 
         case "rs" :
         case "pr":
         case "sp":
-            win();
+            win(userChoice, compChoice);
             break
 
         case "rp" :
         case "ps":
         case "sr":
-            lose();
+            lose(userChoice, compChoice);
             break
     };
 };
@@ -76,3 +92,11 @@ function main(){
 
 main();
 
+
+
+function gameWinner(winner){
+    result_p.innerHTML = `Game Over`;
+    rock_button.removeEventListener('click', playRound('r'));
+    paper_button.removeEventListener('click', playRound('p'));
+    scissors_button.removeEventListener('click', playRound('s'));
+}
